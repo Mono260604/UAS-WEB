41bcfa910c0263f9f984a09cae18379dd47aa440
Mono260604 <naufalx26@gmail.com>
2023-12-18 20:09:15 +0700

    appliying pack

---
+++ front-end/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./NavBar"; // Perhatikan penggunaan huruf kapital pada impor
+
+function App() {
+  return (
+    <>
+      <div>
+        <NavBar />
+      </div>
+      <a>Hello World!</a>
+    </>
+  );
+}
+
+export default App;
---
+++ front-end/src/navBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const NavBar = () => {
+  return (
+    <nav className="bg-red">
+        <li>
+
+        </li>
+    </nav>
+  );
+};
+
+export default NavBar;
